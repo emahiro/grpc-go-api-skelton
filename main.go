61e4b7d005d5fcc370c7b6e9438ae76994daa12c
emahiro <ema.hiro@gmail.com>
2023-06-08 00:13:26 +0900

    add: multi services

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
 
-func main() { fmt.Println("hello world") }
+	"golang.org/x/exp/slog"
+
+	"github.com/emahiro/grpc-go-api-skelton/gen/proto/echo/v1/echov1connect"
+	"github.com/emahiro/grpc-go-api-skelton/gen/proto/greet/v1/greetv1connect"
+	"github.com/emahiro/grpc-go-api-skelton/service"
+)
+
+var addr = ":8080"
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := http.NewServeMux()
+	mux.Handle(greetv1connect.NewGreetServiceHandler(&service.GreeterService{}))
+	mux.Handle(echov1connect.NewEchoServiceHandler(&service.EchoService{}))
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	go func() {
+		<-ctx.Done()
+		if err := server.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	slog.InfoCtx(ctx, "start server", "port", fmt.Sprintf("localhost%s", addr))
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
--- service/greet_service.go
+++ service/greet_service.go
@@ -14,7 +14,7 @@ type GreeterService struct{}
 func (s *GreeterService) Greet(ctx context.Context, req *connect_go.Request[v1.GreetRequest]) (*connect_go.Response[v1.GreetResponse], error) {
 	userName := req.Msg.UserName
 	resp := connect_go.NewResponse(&v1.GreetResponse{
-		Message: fmt.Sprintf("Hello %d", userName),
+		Message: fmt.Sprintf("Hello %s", userName),
 	})
 	return resp, nil
 }
