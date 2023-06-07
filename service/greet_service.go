9e43a74e22f8752b4f35aa623d729ef7d46d1155
emahiro <ema.hiro@gmail.com>
2023-06-08 03:12:14 +0900

    add: error

--- service/greet_service.go
+++ service/greet_service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	"github.com/bufbuild/connect-go"
 	connect_go "github.com/bufbuild/connect-go"
 
 	v1 "github.com/emahiro/grpc-go-api-skelton/gen/proto/greet/v1"
@@ -13,6 +15,9 @@ type GreeterService struct{}
 
 func (s *GreeterService) Greet(ctx context.Context, req *connect_go.Request[v1.GreetRequest]) (*connect_go.Response[v1.GreetResponse], error) {
 	userName := req.Msg.UserName
+	if userName == "" {
+		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("user name is empty"))
+	}
 	resp := connect_go.NewResponse(&v1.GreetResponse{
 		Message: fmt.Sprintf("Hello %s", userName),
 	})
