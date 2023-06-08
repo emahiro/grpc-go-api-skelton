c84afed293d21d071f86adfa7af4cee7f4a80435
emahiro <ema.hiro@gmail.com>
2023-06-08 22:59:02 +0900

    add: intercepter

---
+++ intercepter/intercepter.go
@@ -0,0 +1,18 @@
+package intercepter
+
+import (
+	"context"
+
+	"github.com/bufbuild/connect-go"
+	"golang.org/x/exp/slog"
+)
+
+func NewIntercepter() connect.UnaryInterceptorFunc {
+	intercepter := func(next connect.UnaryFunc) connect.UnaryFunc {
+		return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+			slog.InfoCtx(ctx, "this is intercepter")
+			return next(ctx, req)
+		}
+	}
+	return connect.UnaryInterceptorFunc(intercepter)
+}
