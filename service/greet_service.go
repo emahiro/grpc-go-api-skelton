89193191fa696b8153515ce418d89f97a0a28f76
emahiro <ema.hiro@gmail.com>
2023-06-10 02:37:22 +0900

    add: implement stream service

--- service/greet_service.go
+++ service/greet_service.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	connect "github.com/bufbuild/connect-go"
+	"golang.org/x/exp/slog"
 
 	v1 "github.com/emahiro/grpc-go-api-skelton/gen/proto/greet/v1"
 )
@@ -24,7 +26,22 @@ func (s *GreeterService) Greet(ctx context.Context, req *connect.Request[v1.Gree
 }
 
 func (s *GreeterService) GreetStreaming(ctx context.Context, stream *connect.ClientStream[v1.GreetStreamingRequest]) (*connect.Response[v1.GreetStreamingResponse], error) {
-	return nil, nil
+	slog.InfoCtx(ctx, "request header", "header", stream.RequestHeader())
+	var message strings.Builder
+	for stream.Receive() {
+		g := fmt.Sprintf("Hello, %s\n", stream.Msg().UserName)
+		if _, err := message.WriteString(g); err != nil {
+			return nil, connect.NewError(connect.CodeInternal, err)
+		}
+	}
+	if err := stream.Err(); err != nil {
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+	res := connect.NewResponse(&v1.GreetStreamingResponse{
+		Message: message.String(),
+	})
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
 }
 
 func (s *GreeterService) GreetDidiStreaming(ctx context.Context, stream *connect.BidiStream[v1.GreetDidiStreamingRequest, v1.GreetDidiStreamingResponse]) error {
